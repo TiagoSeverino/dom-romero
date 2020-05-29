8092f4f06039b926dbbf910300d5994fd8c5c678
TiagoSeverino <tiago@severino.xyz>
2020-05-29 15:31:09 +0100

    Updates

---
+++ precache-manifest.a515246ecee12fc2580bdb74a25bf328.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8955098107016fdc1f82514bf77348b6",
+    "url": "/dom-romero/index.html"
+  },
+  {
+    "revision": "b061b588ff60631c784d",
+    "url": "/dom-romero/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "415b864b237720ab9e8e",
+    "url": "/dom-romero/static/css/main.51190ced.chunk.css"
+  },
+  {
+    "revision": "b061b588ff60631c784d",
+    "url": "/dom-romero/static/js/2.0e942500.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/dom-romero/static/js/2.0e942500.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "415b864b237720ab9e8e",
+    "url": "/dom-romero/static/js/main.f7cc56ea.chunk.js"
+  },
+  {
+    "revision": "737c744ba2c60f25ff1b",
+    "url": "/dom-romero/static/js/runtime-main.8c8245e3.js"
+  }
+]);
