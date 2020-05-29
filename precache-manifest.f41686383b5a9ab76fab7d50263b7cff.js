525717107f83df1ab1238670ba0df5b1568df795
TiagoSeverino <tiago@severino.xyz>
2020-05-29 15:28:53 +0100

    Updates

---
+++ precache-manifest.f41686383b5a9ab76fab7d50263b7cff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39735f4c2c657efdac830c21d16572af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b061b588ff60631c784d",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5a08dfc3780667fdf68b",
+    "url": "/static/css/main.51190ced.chunk.css"
+  },
+  {
+    "revision": "b061b588ff60631c784d",
+    "url": "/static/js/2.0e942500.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.0e942500.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a08dfc3780667fdf68b",
+    "url": "/static/js/main.ca43b8b6.chunk.js"
+  },
+  {
+    "revision": "caf2a04453df4bace4e4",
+    "url": "/static/js/runtime-main.009173a0.js"
+  }
+]);
